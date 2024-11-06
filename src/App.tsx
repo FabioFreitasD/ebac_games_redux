8de647d92146143ceddb0ce680cb29e2a4d5fc7b
Fabio <fabiofreitascmav@gmail.com>
2024-11-06 12:24:51 -0300

    primeiro commit

--- src/App.tsx
+++ src/App.tsx
@@ -17,27 +17,12 @@ export type Game = {
 }
 
 function App() {
-  const [games, setGames] = useState<Game[]>([])
-
-  useEffect(() => {
-    fetch('http://localhost:4000/produtos')
-      .then((res) => {
-        console.log('Status da resposta:', res.status) // Log do status da resposta
-        return res.json()
-      })
-      .then((res) => {
-        console.log('Dados recebidos:', res) // Log dos dados recebidos
-        setGames(res)
-      })
-      .catch((error) => console.error('Erro ao buscar produtos:', error))
-  }, [])
-
   return (
     <Provider store={store}>
       <GlobalStyle />
       <div className="container">
         <Header />
-        <Produtos jogos={games} />
+        <Produtos />
       </div>
     </Provider>
   )
